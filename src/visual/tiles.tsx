597849d6388d5e232c0a78bf992dd145f13a45d1
Gen Hames <hamesg@tutanota.de>
2020-09-21 13:11:07 -0700

    Reorganize. Draw lines between mouse moves

--- src/visual/tiles.tsx
+++ src/visual/tiles.tsx
@@ -18,13 +18,6 @@ const {
 // Hard on the eyes if it's too fast...
 const hoverThrottled = throttle(hover, 50);
 
-const colourTile = (x: number, y: number) => {
-  const arr = tileData();
-  if (arr[y][x] === brushColour()) return;
-  arr[y][x] = brushColour();
-  tileData(arr);
-};
-
 const TilesCanvas = (): h.JSX.Element => {
   const canvas = <canvas/> as HTMLCanvasElement;
   const ctx = canvas.getContext('2d');
@@ -50,6 +43,7 @@ const TilesCanvas = (): h.JSX.Element => {
 
     // Outline the tile
     ctx.lineWidth = 0.5;
+    ctx.strokeStyle = 'black';
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.translate(0.5, 0.5);
 
@@ -68,66 +62,104 @@ const TilesCanvas = (): h.JSX.Element => {
     ctx.closePath();
   };
 
-  // let toRedraw = [];
-  subscribe(() => {
+  const drawLine = (x1: number, y1: number, x2: number, y2: number) => {
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+  };
+
+  let rAFLineQueue: { x: number, y: number }[] = [];
+  let rAFTileQueue: { x: number, y: number }[] = [];
+  let rAFSet = false;
+  let brushDown = false;
+  let brushDragging = false;
+
+  const rAFUpdate = () => {
+    console.log('rAFRedrawQueue:', rAFTileQueue.length);
     const data = tileData();
     const size = tileSizePx();
-    // Don't redraw when the count changes: tiles() will update via computed()
-    const cX = sample(tileCountX);
-    const cY = sample(tileCountY);
-
-    // There's a border so +1
-    const canvasWidthPx = cX * size + 1;
-    const canvasHeightPx = cY * size + 1;
-    // If this is a different observable/subscription there's a race condition
-    // where canvas resizing runs _after_ data is drawn. Resizing always clears
-    // a canvas: https://stackoverflow.com/q/11179274/
-    if (canvas.width !== canvasWidthPx || canvas.height !== canvasHeightPx) {
-      console.log('Resizing canvas');
-      canvas.width = canvasWidthPx;
-      canvas.height = canvasHeightPx;
+    for (const c of rAFTileQueue) {
+      drawSquare(c.x, c.y, size, data[c.y][c.x]);
     }
-    console.log('Draw');
-    for (let y = 0; y < cY; y++) {
-      for (let x = 0; x < cX; x++) {
-        drawSquare(x, y, size, data[y][x]);
+    if (rAFLineQueue.length > 1) {
+      let [p] = rAFLineQueue;
+      for (let i = 1; i < rAFLineQueue.length; i++) {
+        const c = rAFLineQueue[i];
+        drawLine(p.x, p.y, c.x, c.y);
+        p = c;
       }
+      rAFLineQueue = [];
     }
-  });
+    rAFTileQueue = [];
+    rAFSet = false;
+  };
 
-  const coord = (ev: MouseEvent) => {
+  const updateTile = (coord: { x: number, y: number }) => {
+    const { x, y } = coord;
+    const arr = tileData();
+    if (arr[y][x] === brushColour()) return;
+    arr[y][x] = brushColour();
+    tileData(arr);
+
+    rAFTileQueue.push(coord);
+    // Call rAF only once since callbacks stack
+    if (!rAFSet) {
+      requestAnimationFrame(rAFUpdate);
+      rAFSet = true;
+    }
+  };
+
+  const evToCoord = (ev: MouseEvent) => {
     const size = tileSizePx();
     const x = Math.floor(ev.offsetX / size);
     const y = Math.floor(ev.offsetY / size);
     return { x, y };
   };
 
-  let brushDown = false;
-  let brushDragging = false;
-  canvas.addEventListener('mousedown', () => {
+  canvas.addEventListener('mousedown', ev => {
     brushDown = true;
+    rAFLineQueue = [{ x: ev.offsetX, y: ev.offsetY }];
   });
+
   canvas.addEventListener('mouseup', ev => {
+    rAFLineQueue.push({ x: ev.offsetX, y: ev.offsetY });
+
+    const c = evToCoord(ev);
+    updateTile(c);
+    click(`(${c.x}, ${c.y})`);
+
     brushDown = false;
-    if (brushDragging) {
-      console.log('Dragend');
-      brushDragging = false;
-      const { x, y } = coord(ev);
-      colourTile(x, y);
-    }
+    if (brushDragging) brushDragging = false;
   });
+
   canvas.addEventListener('mousemove', ev => {
-    const { x, y } = coord(ev);
-    hoverThrottled(`(${ev.offsetX}, ${ev.offsetY}) ➡ (${x}, ${y})`);
+    const c = evToCoord(ev);
+    hoverThrottled(`(${ev.offsetX}, ${ev.offsetY}) ➡ (${c.x}, ${c.y})`);
     if (brushDown) {
       brushDragging = true;
-      colourTile(x, y);
+      updateTile(c);
+      rAFLineQueue.push({ x: ev.offsetX, y: ev.offsetY });
     }
   });
-  canvas.addEventListener('click', ev => {
-    const { x, y } = coord(ev);
-    click(`(${x}, ${y})`);
-    colourTile(x, y);
+
+  // Resizing needs to redraw all tiles https://stackoverflow.com/q/11179274/
+  subscribe(() => {
+    console.log('Resizing canvas');
+    const [cX, cY, size] = [tileCountX(), tileCountY(), tileSizePx()];
+    // Don't track updates to the tile array
+    const data = sample(tileData);
+
+    // There's a border so +1
+    canvas.width = cX * size + 1;
+    canvas.height = cY * size + 1;
+
+    for (let y = 0; y < cY; y++)
+      for (let x = 0; x < cX; x++)
+        drawSquare(x, y, size, data[y][x]);
   });
 
   return canvas;

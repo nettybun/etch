80503dd89c223bf942decfd53aebbe262f608af3
Gen Hames <hamesg@tutanota.de>
2020-12-06 22:54:55 -0800

    Fix for CJS and esbuild

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 
 // ESM doesn't have __dirname anymore
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const dirname = __dirname || path.dirname(new URL(import.meta.url).pathname);
 
 export const PORT = 3000;
-export const CLIENT_SERVE_ROOT = path.resolve(__dirname, '../../client/serve');
+export const CLIENT_SERVE_ROOT = path.resolve(dirname, '../../client/serve');
 export const HEARTBEAT_MESSAGE = '\uD83D\uDC93';
 export const HEARTBEAT_MS = 10000;
 export const WATCHER_DEBOUNCE_MS = 500;
